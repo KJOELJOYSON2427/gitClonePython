1f83ccf275bea87d4df9ae5b280840c5a37dde6b
K Kevin Gladson <joeljoyson2427@gmail.com>
2025-09-10 19:59:45 +0530

    Made a status stages

--- Status.py
+++ Status.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+from typing import Dict, List
+from Blob import Blob
 from Repository import Repository
 from Tree import Tree
 from commit import Commit
@@ -44,18 +47,36 @@ Clean: everything matches HEAD.
                 last_index_files={}
        
         # added = index.keys() - last_index_files.keys()
+        #what files are stagged for commit ->index
+        stagged_files:list[tuple]=[]
         for file_path in set(index.keys()) | set(last_index_files.keys()):
             index_hash = index.get(file_path)
             last_hash = last_index_files.get(file_path)
 
-            if last_hash is None and index_hash is not None:
-              print(f"        new file:   {file_path}")
+            if index_hash and not last_hash:
+              stagged_files.append(("new file", file_path))
+            #   print(f"        new file:   {file_path}")
             if  index_hash and last_hash and last_hash != index_hash:
-                print(f"        modified:   {file_path}")
+                stagged_files.append(("modified ", file_path))
+                # print(f"        modified:   {file_path}")
 
-
-        #what files are stagged for commit ->index
-        #what files have modified but not staged
+        if stagged_files:
+             print("\nChanged to be committed:")
+             for (stagged_status, file_path) in sorted(stagged_files):
+               print(f"       {stagged_status}:  {file_path}")
+        
+        #what files have modified but not staged -> once commited in but in current working directory it is changed but not staged
+        unstaged_files=[]
+        print("log1")
+        working_dirs_files=self.get_working_directory()
+        for file_path in working_dirs_files:
+            if file_path in index:
+                if working_dirs_files[file_path] !=working_dirs_files[file_path]:
+                    unstaged_files.add(file_path)
+        if unstaged_files:
+             print("\nChanges not staged for commit:\n   use \"git add <file>...\" to update what will be committed\n    use \"git restore <file>...\" to discard changes in working directory")
+             for  file_path in sorted(unstaged_files):
+               print(f"      modified: {file_path}")            
         #what files are untracked
         #what files have been delete
         pass  
@@ -80,3 +101,41 @@ Clean: everything matches HEAD.
 
         walk_tree(tree_hash)
         return index
+    
+
+
+#             [
+#         PosixPath('repo/main.py'),
+#     PosixPath('repo/utils/helper.py'),
+#     PosixPath('repo/utils/nested/deep_file.py'),
+#     PosixPath('repo/data/sample.txt')
+# ]
+            
+    def get_all_files(self)->List[Path]:
+        files =[]
+
+        for item in self.repo.path.rglob("*"):
+            if [".git", "__pycache__"] in item.parts:
+                continue
+
+            if item.is_file():
+                files.append(item)
+        return files          
+
+    #figure out all the  files present within the working directory
+    #dictionary mapping
+    def get_working_directory(self)->Dict[str,str]:
+        working_dir_files:Dict[str,str]={}
+        files_of_dir:List[Path]=self.get_all_files()
+        for item in files_of_dir:
+            rel_path=str(item.relative_to(self.repo.path))
+            
+            try:
+                content = item.read_bytes()
+                blob =Blob(content)
+                working_dir_files[rel_path]=blob.hash() # *init called the content
+                
+            except Exception:
+                continue
+        print("log2")    
+        return working_dir_files    
